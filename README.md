6a0d92401f9948d7f366bffcd1df5016fc3b9a18
mojaeb <mojaebi@gmail.com>
2021-09-01 16:15:50 +0430

    feat: send and receive signals between goroutines

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 <br>
 you can switch between branched for learn this sections
+
+
 ```go
 channel-intro   // send message from goroutine
 waitgroup       // simple use wait group  
---
+++ channels/message.go
@@ -0,0 +1,17 @@
+import (
+	"fmt"
+)
+
+func sender(message chan<- string) {
+	message <- "hello"
+}
+
+func main() {
+	message := make(chan string)
+
+	go sender(message)
+	go sender(message)
+
+	fmt.Println(<-message)
+	fmt.Println(<-message)
+}
